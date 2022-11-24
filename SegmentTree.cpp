2d1ca74d06c0eeb9d9a5ac741553decf5803011e
charleslakes <carloslagoscortes@hotmail.com>
2022-11-24 03:05:18 -0300

    add orderer_set

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,20 +1,39 @@
-const int N = 1e5;  // limit for array size
-int n;  // array size
-int t[2 * N];
+//#include <bits/stdc++.h>
+//using namespace std;
+vector<int> v, Tree;
+void buildTree(int tidx, int lo, int hi){
+	if(lo==hi){
+		Tree[tidx] = v[hi];
+		return;
+	}
+	int mid = (lo+hi)/2;
+	buildTree(2*tidx+1, lo, mid);
+	buildTree(2*tidx+2, mid+1, hi);
+	Tree[tidx] = Tree[2*tidx+1] + Tree[2*tidx+2];
 
-void build() {  // build the tree
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
 }
 
-void modify(int p, int value) {  // set value at position p
-  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+void updateTree(int tidx, int lo, int hi, int idx, int val){
+	if(lo==hi){
+		Tree[tidx] = val;
+		return;
+	}
+	int mid = (lo+hi)/2;
+	if(idx<=mid)
+		updateTree(2*tidx+1, lo, mid, idx, val);
+	else
+		updateTree(2*tidx+2, mid+1, hi, idx, val);
+	Tree[tidx] = Tree[2*tidx+1] + Tree[2*tidx+2];
 }
 
-int query(int l, int r) {  // sum on interval [l, r)
-  int res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
-  }
-  return res;
+int query(int tidx, int lo, int hi, int L, int R){
+	if( R<lo || L>hi )
+		return 0;
+	if( L<=lo && hi<=R )
+		return Tree[tidx];
+	int mid = (lo+hi)/2;
+	int leftans = query(2*tidx+1, lo, mid, L, R);
+	int rightans = query(2*tidx+2, mid+1, hi, L, R);
+	return leftans + rightans;
+
 }
